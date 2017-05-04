b9f694c3e1fe090146dd01cd7275c6a6fca3690f
Y Okumura <y-okumura@nichiwa-system.co.jp>
2017-05-04 16:42:33 +0900

    移動とズームに火の粉がついてくる

--- js/wind-layer.js
+++ js/wind-layer.js
@@ -21,41 +21,78 @@ function createWindLayerInto(map) {
         d3.select(this).remove();
       });
   }
+  var timers = []
+  function clearTimer() {
+    timers.forEach(function(timer) {
+       clearInterval(timer);
+    });
+    timers = [];
+  }
+
+    function fitLayer() {
+      var bounds = map.getBounds();
+      var topLeft = map.latLngToLayerPoint(bounds.getNorthWest());
+      var bottomRight = map.latLngToLayerPoint(bounds.getSouthEast());
+
+      svgLayer.attr("width", bottomRight.x - topLeft.x)
+        .attr("height", bottomRight.y - topLeft.y)
+        .style("left", topLeft.x + "px")
+        .style("top", topLeft.y + "px");
+
+      plotLayer.attr('transform', 'translate('+ -topLeft.x + ',' + -topLeft.y + ')');
+    }
+
+  function loadData(fireData, windData) {
+
+    function reset() {
+      clearTimer();
+      fitLayer();
+      loadData(fireData, windData);
+    }
+
+    map.on("zoom", function() {
+      reset();
+    });
+
+    map.on("moveend", function() {
+      reset();
+    });
+
+    $.getJSON(windData, function(wind) {
+      $.getJSON(fireData, function(data) {
+        data.forEach(function(fire) {
+          var pos = map.latLngToLayerPoint(new L.LatLng(fire.lat, fire.lng));
+          var interval = fire.value > 400 ? 1000 : fire.value > 300 ? 5000 - fire.value*10 : 2000
+
+
+          var size = 10;
+          var rect = {left:pos.x-size, top: pos.y-size, right:pos.x + size, bottom:pos.y+size}
+
+          // 風速: ダミー
+          var windDir = wind[0].data[(90-Math.round(fire.lat))*360 + Math.round(fire.lng)];
+          var v = {
+            x:Math.cos(windDir * Math.PI / 180)*50,
+            y:Math.sin(windDir * Math.PI / 180)*50
+          };
+
+          timers.push(setInterval(
+            function() {
+              putParticle(svgLayer,
+                d3.randomUniform(rect.left, rect.right),
+                d3.randomUniform(rect.top, rect.bottom),
+                v.x, v.y
+              );
+            },
+            interval
+          ));
+        });
+      });
+    });
+  };
 
   return {
     svgLayer: svgLayer,
     plotLayer: plotLayer,
-    loadData: function(fireData, windData) {
-      $.getJSON(windData, function(wind) {
-        $.getJSON(fireData, function(data) {
-          data.forEach(function(fire) {
-            var pos = map.latLngToLayerPoint(new L.LatLng(fire.lat, fire.lng));
-            var interval = fire.value > 400 ? 1000 : fire.value > 300 ? 5000 - fire.value*10 : 2000
-
-
-            var size = 10;
-            var rect = {left:pos.x-size, top: pos.y-size, right:pos.x + size, bottom:pos.y+size}
-
-            // 風速: ダミー
-            var windDir = wind[0].data[(90-Math.round(fire.lat))*360 + Math.round(fire.lng)];
-            var v = {
-              x:Math.cos(windDir * Math.PI / 180)*50,
-              y:Math.sin(windDir * Math.PI / 180)*50
-            };
-
-            setInterval(
-              function() {
-                putParticle(svgLayer,
-                  d3.randomUniform(rect.left, rect.right),
-                  d3.randomUniform(rect.top, rect.bottom),
-                  v.x, v.y
-                );
-              },
-              interval
-            )
-          });
-        });
-      });
-    }
+    loadData: loadData
   }
 }
