d32eb2e25a575418c26923d84e33fec786341644
Y Okumura <y-okumura@nichiwa-system.co.jp>
2018-11-16 11:03:09 +0900

    火災データを日付ごとに出力するように手を入れていたファイルを発掘したので、更新しておきます

--- convert.groovy
+++ convert.groovy
@@ -1,12 +1,26 @@
-new File('data/fire.json').withPrintWriter {p->
-    p.println('[');
-    def firstLine = true;
-    new File('MODIS_C6_Global_24h.csv').eachLine {
-        if (firstLine) {
-            firstLine = false;
+def dataDir = new File('data/fire/')
+dataDir.mkdirs()
+
+def printers = [:]
+
+def firstLine = true;
+new File('VNP14IMGTDL_NRT_Global_7d-2.csv').eachLine {
+    if (firstLine) {
+        firstLine = false;
+    } else {
+        String[] line = it.split(',')
+        def date = line[5]
+        if (! printers.containsKey(date)) {
+            printers[date] = new File(dataDir, "${date}.json").newPrintWriter()
+            printers[date].println('[');
         } else {
-            p.println(it.replaceFirst(/^([^,]+),([^,]+),([^,]+),.*$/, '{"lat": $1, "lng": $2, "value": $3},'))
+            printers[date].println(',');
         }
+        printers[date].print("""  {"lat": ${line[0]}, "lng": ${line[1]}, "value": ${line[2]}}""")
     }
+}
+printers.each{d,p->
+    p.println();
     p.println(']');
+    p.close();
 }
